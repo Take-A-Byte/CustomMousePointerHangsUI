840dc8b856f4d96bc76ff0f7a468bc203ea2e948
Shantanu Methikar <smethikar18@gmail.com>
2023-05-24 13:17:04 +0530

    Add html to check responsiveness of html

--- CustomMousePointerHangsUI/MainWindow.xaml.cs
+++ CustomMousePointerHangsUI/MainWindow.xaml.cs
@@ -1,36 +1,24 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
+using Microsoft.Web.WebView2.Core;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace CustomMousePointerHangsUI
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainWindow : Window
     {
         public MainWindow()
         {
-            this.InitializeComponent();
-        }
+            InitializeComponent();
+            webView2.Loaded += async (s, e) =>
+            {
+                await webView2.EnsureCoreWebView2Async().AsTask();
+                webView2.CoreWebView2.SetVirtualHostNameToFolderMapping(hostName: "shan.tutorials.winui3",
+                folderPath: "Assets",
+                accessKind: CoreWebView2HostResourceAccessKind.Allow);
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
-        {
-            myButton.Content = "Clicked";
+                webView2.Source = new Uri("https://shan.tutorials.winui3/html/index.html");
+            };
         }
     }
 }
